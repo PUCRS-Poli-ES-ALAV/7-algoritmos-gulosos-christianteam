c634317ce1c7d1092d5815b62990b9f3b08f6e88
christianKossmann2741 <62452600+christianKossmann2741@users.noreply.github.com>
2022-04-06 20:25:05 -0300

    Feito 2

--- atividade.py
+++ atividade.py
@@ -18,5 +18,19 @@ def troco(listaDeMoedas, valor):
 listaDeMoedas = [100, 25, 10, 5, 1]
 valor = 2.89
 
-print(troco(listaDeMoedas, valor))
+def intervaloTerminator(listaDeIntervalos):
+    maiorAt = -2147483648
+    res = []
+    for intervalo in listaDeIntervalos:
+        if(intervalo[0] > maiorAt):
+            res.append(intervalo)
+            maiorAt = intervalo[1]
 
+    return res
+
+print(troco(listaDeMoedas, valor))
+print("\n\n____________________________\n\n")
+lista = [(4,5),(2,4),(1,6),(6,7),(4,8),(7,10),(9,11),(9,12),(3,13),(13,14)]
+lista.sort(key=lambda tup: tup[1]) #Organiza a lista baseado no segundo item das tuplas
+print(lista)
+print(intervaloTerminator(lista))
