3aa3a48d9578a26f29fd59c4ae9a1e2425617724
christianKossmann2741 <62452600+christianKossmann2741@users.noreply.github.com>
2022-04-13 20:12:55 -0300

    Comeco no nrainhas

--- atividade.py
+++ atividade.py
@@ -1,3 +1,28 @@
+def preenche(posAt, tamanho):
+    
+
+def nrainhas(tamanho, rainhas):
+    linhas = [0]*tamanho #Cria uma lista com X (tamanho) zeros
+    mat = [linhas]*tamanho #Cria uma matriz com base na entrada de "linha"
+    nRainhasRestantes = tamanho
+    while(nRainhasRestantes > 0):
+        posAt = (0,0)
+        achou = False
+        while(achou == False):
+            if(mat[posAt[0]][posAt[1]] == 0):
+                mat[posAt[0]][posAt[1]] = -1
+                preenche(posAt, tamanho)
+                achou = True    
+            if(posAt[0] < tamanho-1 and posAt[1] < (tamanho-1)) #impede que o posAt aponte para uma posisão invalida
+                if(posAt[0] < tamanho-1):
+                    posAt[0]+=1
+                else:
+                    posAt[1]+=1
+                    posAt[0] = 0
+            else:
+                #F****
+    #print(mat)
+
 def troco(listaDeMoedas, valor):
     iteracoes = 0
     valor = valor * 100
@@ -34,3 +59,5 @@ lista = [(4,5),(2,4),(1,6),(6,7),(4,8),(7,10),(9,11),(9,12),(3,13),(13,14)]
 lista.sort(key=lambda tup: tup[1]) #Organiza a lista baseado no segundo item das tuplas
 print(lista)
 print(intervaloTerminator(lista))
+
+nrainhas(5,5)
